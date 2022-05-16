5a6ac01b910c989754d6cb9ba753dcb3adc25a72
ilyas.bekpayev <ilyas.bekpayev@gmail.com>
2022-05-16 12:51:22 +0600

    logic finished

--- src/js/app-reg.js
+++ src/js/app-reg.js
@@ -9,10 +9,83 @@ const passwordRepeatInput = document.querySelector("#password-repeat-input");
 
 regBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  checkNameSurname(nameInput);
-  checkNameSurname(surnameInput);
-  checkLogin(loginInput);
+  const validName = checkNameSurname(nameInput);
+  const validSurName = checkNameSurname(surnameInput);
+  const validLogin = checkLogin(loginInput);
+  const validEmail = checkEmail(emailInput);
+  const validPhone = checkPhone(phoneNumberInput);
+  const validPassword = checkPassword(passwordInput);
+  const validPaswwordRepeat = checkPasswordRepeat(
+    passwordInput,
+    passwordRepeatInput
+  );
+  if (
+    validEmail &&
+    validSurName &&
+    validLogin &&
+    validName &&
+    validPassword &&
+    validPhone &&
+    validPaswwordRepeat
+  ) {
+    sumbitFormObject();
+  }
+});
+
+emailInput.addEventListener("focus", () => {
   checkEmail(emailInput);
+});
+
+emailInput.addEventListener("blur", () => {
+  checkEmail(emailInput);
+});
+
+loginInput.addEventListener("focus", () => {
+  checkLogin(loginInput);
+});
+
+loginInput.addEventListener("blur", () => {
+  checkLogin(loginInput);
+});
+
+nameInput.addEventListener("focus", () => {
+  checkNameSurname(nameInput);
+});
+
+nameInput.addEventListener("blur", () => {
+  checkNameSurname(nameInput);
+});
+
+surnameInput.addEventListener("focus", () => {
+  checkNameSurname(surnameInput);
+});
+
+surnameInput.addEventListener("blur", () => {
+  checkNameSurname(surnameInput);
+});
+
+phoneNumberInput.addEventListener("focus", () => {
+  checkPhone(phoneNumberInput);
+});
+
+phoneNumberInput.addEventListener("blur", () => {
+  checkPhone(phoneNumberInput);
+});
+
+passwordInput.addEventListener("focus", () => {
+  checkPassword(passwordInput);
+});
+
+passwordInput.addEventListener("blur", () => {
+  checkPassword(passwordInput);
+});
+
+passwordRepeatInput.addEventListener("focus", () => {
+  checkPasswordRepeat(passwordInput, passwordRepeatInput);
+});
+
+passwordRepeatInput.addEventListener("blur", () => {
+  checkPasswordRepeat(passwordInput, passwordRepeatInput);
 });
 
 function checkNameSurname(input) {
@@ -20,8 +93,10 @@ function checkNameSurname(input) {
   if (!pattern.test(input.value)) {
     input.nextSibling.innerText =
       "Must be filled. Only latin, russian letters and hyphen sign";
+    return false;
   } else {
     input.nextSibling.innerText = "";
+    return true;
   }
 }
 
@@ -31,12 +106,15 @@ function checkLogin(input) {
   if (onlyNumberLogin.test(input.value)) {
     input.nextSibling.innerText = "";
     input.nextSibling.innerText = "Cannot contain only numbers";
+    return false;
   } else if (!pattern.test(input.value)) {
     input.nextSibling.innerText = "";
     input.nextSibling.innerText =
       "Must be filled. Length from 3 to 15 symbol. Only latin letters and - / signs";
+    return false;
   } else {
     input.nextSibling.innerText = "";
+    return true;
   }
 }
 
@@ -46,7 +124,59 @@ function checkEmail(input) {
   if (!pattern.test(input.value)) {
     input.nextSibling.innerText = "";
     input.nextSibling.innerText = "Must contain proper email format";
-  } else input.nextSibling.innerText = "";
+    return false;
+  } else {
+    input.nextSibling.innerText = "";
+    return true;
+  }
 }
 
-function checkPhone(input) {}
+function checkPhone(input) {
+  const pattern = /^(\+)?([0-9]{8,15})$/;
+  if (!pattern.test(input.value)) {
+    input.nextSibling.innerText = "";
+    input.nextSibling.innerText =
+      "Must containe proper phone format. 8 - 15 digits only";
+    return false;
+  } else {
+    input.nextSibling.innerText = "";
+    return true;
+  }
+}
+
+function checkPassword(input) {
+  const pattern =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,30}$/;
+  if (!pattern.test(input.value)) {
+    input.nextSibling.innerText = "";
+    input.nextSibling.innerText =
+      "8 - 30 symbols only, at least 1 number and 1 special character";
+    return false;
+  } else {
+    input.nextSibling.innerText = "";
+    return true;
+  }
+}
+
+function checkPasswordRepeat(pass1, pass2) {
+  if (pass1.value !== pass2.value) {
+    pass2.nextSibling.innerText = "";
+    pass2.nextSibling.innerText = "Password does not match";
+    return false;
+  } else {
+    pass2.nextSibling.innerText = "";
+    return true;
+  }
+}
+
+function sumbitFormObject() {
+  const formData = {
+    email: emailInput.value,
+    login: loginInput.value,
+    name: nameInput.value,
+    surname: surnameInput.value,
+    phone: phoneNumberInput.value,
+    password: passwordInput.value,
+  };
+  console.log(formData);
+}
